706da5f194f04b13314cac790a31efb4bc41248d
Zack Grossbart <zack@grossbart.com>
2011-09-11 17:28:18 -0400

    Added a little more to the chain function unit test.

--- src/test/resources/propertyassignmentchain.js
+++ src/test/resources/propertyassignmentchain.js
@@ -4,4 +4,12 @@ obj1.func1 = obj1.func2 = obj1.func3 = function() {
         return "I'm function 1, 2, and 3";
 };
 
+obj1.func4 = obj1.func5 = obj1.func6 = function() {
+        return "I'm function 4, 5, and 6";
+};
+
+obj1.func7 = function() {
+        return obj1.func6();
+};
+
 obj1.func1();
